71a97300c04b426d5dbd0f7b262475123e7e14c8
imalishehara <146943438+imalishehara@users.noreply.github.com>
2025-07-18 09:42:36 +0530

    Add files via upload

---
+++ Services/ISymptomsServices.cs
@@ -0,0 +1,14 @@
+﻿using Disaster_demo.Models.Entities;
+
+namespace Disaster_demo.Services
+{
+    public interface ISymptomsServices
+    {
+        Task createSymptoms(Symptoms symptoms);
+        Task<List<Symptoms>> GetDsApprovedSymptomsByDistrictAsync(string district);
+
+        Task<List<Symptoms>> GetPendingReportsByDivisionAsync(string dsDivision);
+        List<Symptoms> GetPendingSymptomsByDsDivision(string dsDivision);
+        bool UpdateSymptomStatus(int reportId, string status);
+    }
+}
