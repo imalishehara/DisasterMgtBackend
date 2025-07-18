3b2d2a382298c09751490c309831e88081d91280
imalishehara <146943438+imalishehara@users.noreply.github.com>
2025-07-18 09:41:10 +0530

    Add files via upload

---
+++ Services/IAidRequestServices.cs
@@ -0,0 +1,16 @@
+﻿using Disaster_demo.Models.Entities;
+
+namespace Disaster_demo.Services
+{
+    public interface IAidRequestServices
+    {
+        //Task createAidRequest(AidRequests aidrequests);
+        Task<bool> CreateAidRequestAsync(AidRequests request);
+        Task<List<AidRequests>> GetAidRequestsByDistrict(string district);
+        Task<List<AidRequests>> GetAllDmcRelatedAidRequests(string district);
+        Task<List<AidRequests>> GetDmcApprovedAidRequests();
+        Task<List<AidRequests>> getPendingAidRequests(string dsDivision);
+        bool UpdateStatus(StatusUpdateModel model);
+        
+    }
+}
