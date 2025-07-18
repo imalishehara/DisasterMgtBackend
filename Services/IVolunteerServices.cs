2deded5125c2f0460870350913872fffe042fe2e
imalishehara <146943438+imalishehara@users.noreply.github.com>
2025-07-18 09:43:09 +0530

    Add files via upload

---
+++ Services/IVolunteerServices.cs
@@ -0,0 +1,15 @@
+﻿using Disaster_demo.Models.Entities;
+
+namespace Disaster_demo.Services
+{
+    public interface IVolunteerServices
+    {
+        Task<LoginResponseDTO?> GetVolunteerDetailsAsync(string userId);
+        Task<List<Volunteer>> GetVolunteersByGnDivisionAsync(string dsDivision);
+        Task<int> SignupAsync(VolunteerSignupDTO dto);
+        Task<List<Volunteer>> GetVolunteersByDistrictAsync(string district);
+        Task<bool> UpdateAvailabilityAsync(int userId, AvailabilityStatus newStatus);
+
+
+    }
+}
